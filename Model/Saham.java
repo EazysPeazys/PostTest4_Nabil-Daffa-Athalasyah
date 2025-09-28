e407b09144a0fc2395fab0fe7c5c1bf2e9df3645
Athalasyah Nabil <athalasyahnabil@gmail.com>
2025-09-28 18:00:53 +0800

    Add files via upload

---
+++ Model/Saham.java
@@ -0,0 +1,36 @@
+package Model;
+
+public abstract class Saham extends AsetInvestasi {
+    private String perusahaan;
+
+    public Saham(String nama, double nilai, String perusahaan) {
+        super(nama, nilai, "Saham");
+        this.perusahaan = perusahaan;
+    }
+
+    public String getPerusahaan() {
+        return perusahaan;
+    }
+
+    public void setPerusahaan(String perusahaan) {
+        this.perusahaan = perusahaan;
+    }
+
+    @Override
+    public void infoAset() {
+        System.out.println("Ini merupakan Aset Saham.");
+        System.out.println("Nama Aset: " + getNama());
+        System.out.println("Nilai Aset: " + getNilai());
+        System.out.println("Perusahaan: " + perusahaan);
+    }
+
+    // Overloading
+    @Override
+    public void infoAset(String keterangan) {
+        System.out.println("Saham: " + getNama() + " - " + perusahaan + " (" + keterangan + ")");
+    }
+
+    public void infoAset(int jumlahLot) {
+        System.out.println("Saham " + getNama() + " (" + perusahaan + ") dengan jumlah lot: " + jumlahLot);
+    }
+}
